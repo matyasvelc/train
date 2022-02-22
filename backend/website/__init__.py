d2543841f6f2130d06941aab32c367d318f21ed9
VojtaPoupa <vojta.poupa@email.cz>
2022-02-22 21:18:11 +0100

    2

--- backend/website/__init__.py
+++ backend/website/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
     app.config['SECRET_KEY'] = 'ahoj'
 
     from .views import views
